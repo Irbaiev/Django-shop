54bbac69a4f7b2f15582d5eea02710f68446b0c9
irbaiev <a.irbaeiv75@gmail.com>
2022-04-17 15:31:56 +0300

    Create new registr form

--- accounts/views.py
+++ accounts/views.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 from django.views.generic import CreateView
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import RegisterUserForm
 
 class Singup(CreateView):
-    form_class = UserCreationForm
+    form_class = RegisterUserForm
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
