a88e7ca1d4b799e01271888bbb85348abafdd9fe
Irbaiev <96471596+Irbaiev@users.noreply.github.com>
2022-05-09 11:40:09 +0300

    Edit password

--- shop/settings.py
+++ shop/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'shop_db',
         'USER' : 'postgres',
-        'PASSWORD' : 'Adam2004',
+        'PASSWORD' : 'postgres',
         'HOST' : 'localhost',
         'PORT' : '5432',
     }
